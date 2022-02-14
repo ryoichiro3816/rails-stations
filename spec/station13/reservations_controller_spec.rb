8c089b43c720a6d818534964494427f0864848b5
ryoichiro3816 <ryoichiro3816@gmail.com>
2022-02-14 10:26:08 +0900

    create Screen model

--- spec/station13/reservations_controller_spec.rb
+++ spec/station13/reservations_controller_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 RSpec.describe ReservationsController, type: :controller do
   render_views
   describe 'Station13 GET /movies/:movie_id/schedules/:schedule_id/reservations/new' do
-    let(:failure_request) { get :new, params: { movie_id: 2, schedule_id: 19 }, session: {} }
+    let(:failure_request) { get :new, params: { movie_id: 1, schedule_id: 18 }, session: {} }
 
     it 'date, sheet_idのどちらかまたは両方が渡されていないとき200を返していないこと' do
       failure_request
