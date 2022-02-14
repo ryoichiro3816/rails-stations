a9c04a7a8cd21d6ae0e0ce19f631aa83f70ac08e
ryoichiro3816 <ryoichiro3816@gmail.com>
2022-02-14 10:35:25 +0900

    update Screen model

--- spec/station13/reservations_controller_spec.rb
+++ spec/station13/reservations_controller_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 # 予約に関してこれまでの仕様が満たされていることのテスト
 RSpec.describe ReservationsController, type: :controller do
   render_views
-  # describe 'Station13 GET /movies/:movie_id/schedules/:schedule_id/reservations/new' do
-  #   let(:failure_request) { get :new, params: { movie_id: 1, schedule_id: 1 }, session: {} }
+  describe 'Station13 GET /movies/:movie_id/schedules/:schedule_id/reservations/new' do
+    let(:failure_request) { get :new, params: { movie_id: 1, schedule_id: 1 }, session: {} }
 
-  #   it 'date, sheet_idのどちらかまたは両方が渡されていないとき200を返していないこと' do
-  #     failure_request
-  #     expect(response).not_to have_http_status(200)
-  #   end
-  # end
+    it 'date, sheet_idのどちらかまたは両方が渡されていないとき200を返していないこと' do
+      failure_request
+      expect(response).not_to have_http_status(200)
+    end
+  end
 end
